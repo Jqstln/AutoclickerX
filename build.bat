464760f733adcb2ba3187c504fb1baa9ce1f9adc
Justinnn <hello@justinnn.dev>
2025-04-11 20:16:23 +0200

    // change some comments

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
 @echo off
 echo Starting build process for AutoclickerX...
 
-:: Make sure previous builds are cleared
+:: make sure the paths exists and are correct, if yes then delete them
 IF EXIST build rmdir /s /q build
 IF EXIST dist rmdir /s /q dist
 IF EXIST main.spec del /q main.spec
 
-:: Run PyInstaller with UPX compression using python -m
+:: run pyinstaller with the specified options
 python -m PyInstaller ^
   --name AutoclickerX ^
   --icon=assets\logo_mouse.ico ^
